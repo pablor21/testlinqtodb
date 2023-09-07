391de54092e1047ff6fefaeeb55884569fd2db2c
Pablo Ramírez <pablo@pramirez.dev>
2023-09-06 21:31:00 -0500

    Initial commit

---
+++ DB/Linq2dbSettings.cs
@@ -0,0 +1,41 @@
+﻿using LinqToDB.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ2DB_MVC_Core_5.DB
+{
+    public class Linq2dbSettings : ILinqToDBSettings
+    {
+        public string DefaultConfiguration => "SqlServer";
+        public string DefaultDataProvider => "SqlServer";
+        public readonly IConnectionStringSettings mConnectionStringSettings;
+        public Linq2dbSettings(IConfiguration configuration)
+        {
+            // Figure out the database name from the connection string.
+            var sDBConnection = configuration["ConnectionStrings:DefaultConnection"];
+            var sProviderName = configuration["Authentication:Linq2db:ProviderName"] ?? "";
+            if (sProviderName.Length == 0)
+            {
+                sProviderName = DefaultDataProvider;
+            }
+
+            mConnectionStringSettings = new ConnectionStringSettings
+            {
+                Name = "DefaultConnection",
+                ProviderName = sProviderName,
+                ConnectionString = sDBConnection
+            };
+        }
+
+        public IEnumerable<IDataProviderSettings> DataProviders => Enumerable.Empty<IDataProviderSettings>();
+
+        public IEnumerable<IConnectionStringSettings> ConnectionStrings
+        {
+            get
+            {
+                yield return mConnectionStringSettings;
+            }
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LINQ2DB_MVC_Core_5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
